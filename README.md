67ce4ab3ca8b8a7f8b55715de626b9ed5f39cc77
David DeSandro <desandrocodes@gmail.com>
2014-05-08 07:40:11 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# packery layout mode for Isotope
+
+See [docs for packery layout mode](http://isotope.metafizzy.co/beta/layout-modes/packery.html)
+
+``` js
+$('#container').isotope({
+  layoutMode: 'packery'
+});
+```
---
+++ packery.js
@@ -0,0 +1,71 @@
+/*!
+ * Packery layout mode
+ * sub-classes Packery
+ * http://packery.metafizzy.co
+ */
+
+/*jshint browser: true, strict: true, undef: true, unused: true */
+
+( function( window ) {
+
+'use strict';
+
+// -------------------------- helpers -------------------------- //
+
+// extend objects
+function extend( a, b ) {
+  for ( var prop in b ) {
+    a[ prop ] = b[ prop ];
+  }
+  return a;
+}
+
+// -------------------------- masonryDefinition -------------------------- //
+
+// used for AMD definition and requires
+function packeryDefinition( LayoutMode, Packery ) {
+  // create an Outlayer layout class
+  var PackeryMode = LayoutMode.create('packery');
+
+  // save on to these methods
+  var _getElementOffset = PackeryMode.prototype._getElementOffset;
+  // var layout = PackeryMode.prototype.layout;
+  var _getMeasurement = PackeryMode.prototype._getMeasurement;
+
+  // sub-class Masonry
+  extend( PackeryMode.prototype, Packery.prototype );
+
+  // set back, as it was overwritten by Packery
+  PackeryMode.prototype._getElementOffset = _getElementOffset;
+  // PackeryMode.prototype.layout = layout;
+  PackeryMode.prototype._getMeasurement = _getMeasurement;
+
+  // HACK copy over isOriginLeft/Top options
+  var _manageStamp = PackeryMode.prototype._manageStamp;
+  PackeryMode.prototype._manageStamp = function() {
+    this.options.isOriginLeft = this.isotope.options.isOriginLeft;
+    this.options.isOriginTop = this.isotope.options.isOriginTop;
+    _manageStamp.apply( this, arguments );
+  };
+
+  return PackeryMode;
+}
+
+// -------------------------- transport -------------------------- //
+
+if ( typeof define === 'function' && define.amd ) {
+  // AMD
+  define( [
+      '../layout-mode',
+      'packery/packery'
+    ],
+    packeryDefinition );
+} else {
+  // browser global
+  packeryDefinition(
+    window.Isotope.LayoutMode,
+    window.Packery
+  );
+}
+
+})( window );
