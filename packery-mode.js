da979a3980eb52757f55b47f27685005aa88fa29
David DeSandro <desandrocodes@gmail.com>
2016-04-06 12:01:16 -0400

    ⬆️ update for Packery v2, Isotope v3
    + remove getSize
    + use ._getOption
    +

--- packery-mode.js
+++ packery-mode.js
@@ -13,88 +13,77 @@
     // AMD
     define( [
         'isotope/js/layout-mode',
-        'packery/js/packery',
-        'get-size/get-size'
+        'packery/js/packery'
       ],
       factory );
-  } else if ( typeof exports == 'object' ) {
+  } else if ( typeof module == 'object' && module.exports ) {
     // CommonJS
     module.exports = factory(
       require('isotope-layout/js/layout-mode'),
-      require('packery'),
-      require('get-size')
+      require('packery')
     );
   } else {
     // browser global
     factory(
       window.Isotope.LayoutMode,
-      window.Packery,
-      window.getSize
+      window.Packery
     );
   }
 
 }( window, function factor( LayoutMode, Packery, getSize ) {
 'use strict';
 
-// -------------------------- helpers -------------------------- //
-
-// extend objects
-function extend( a, b ) {
-  for ( var prop in b ) {
-    a[ prop ] = b[ prop ];
-  }
-  return a;
-}
-
-// -------------------------- masonryDefinition -------------------------- //
-
   // create an Outlayer layout class
   var PackeryMode = LayoutMode.create('packery');
+  var proto = PackeryMode.prototype;
 
-  // save on to these methods
-  var _getElementOffset = PackeryMode.prototype._getElementOffset;
-  // var layout = PackeryMode.prototype.layout;
-  var _getMeasurement = PackeryMode.prototype._getMeasurement;
+  var keepModeMethods = {
+    _getElementOffset: true,
+    _getMeasurement: true
+  };
 
-  // sub-class Masonry
-  extend( PackeryMode.prototype, Packery.prototype );
-
-  // set back, as it was overwritten by Packery
-  PackeryMode.prototype._getElementOffset = _getElementOffset;
-  // PackeryMode.prototype.layout = layout;
-  PackeryMode.prototype._getMeasurement = _getMeasurement;
+  // inherit Packery prototype
+  for ( var method in Packery.prototype ) {
+    // do not inherit mode methods
+    if ( !keepModeMethods[ method ] ) {
+      proto[ method ] = Packery.prototype[ method ];
+    }
+  }
 
   // set packery in _resetLayout
-  var _resetLayout = PackeryMode.prototype._resetLayout;
-  PackeryMode.prototype._resetLayout = function() {
+  var _resetLayout = proto._resetLayout;
+  proto._resetLayout = function() {
     this.packer = this.packer || new Packery.Packer();
     _resetLayout.apply( this, arguments );
   };
 
-  var _getItemLayoutPosition = PackeryMode.prototype._getItemLayoutPosition;
-  PackeryMode.prototype._getItemLayoutPosition = function( item ) {
+  var _getItemLayoutPosition = proto._getItemLayoutPosition;
+  proto._getItemLayoutPosition = function( item ) {
     // set packery rect
     item.rect = item.rect || new Packery.Rect();
     return _getItemLayoutPosition.call( this, item );
   };
 
-  // HACK copy over isOriginLeft/Top options
-  var _manageStamp = PackeryMode.prototype._manageStamp;
-  PackeryMode.prototype._manageStamp = function() {
-    this.options.isOriginLeft = this.isotope.options.isOriginLeft;
-    this.options.isOriginTop = this.isotope.options.isOriginTop;
-    _manageStamp.apply( this, arguments );
+  // needsResizeLayout for vertical or horizontal
+  var _needsResizeLayout = proto.needsResizeLayout;
+  proto.needsResizeLayout = function() {
+    if ( this._getOption('horizontal') ) {
+      return this.needsVerticalResizeLayout();
+    } else {
+      return _needsResizeLayout.call( this );
+    }
   };
 
-  PackeryMode.prototype.needsResizeLayout = function() {
-    // don't trigger if size did not change
-    var size = getSize( this.element );
-    // check that this.size and size are there
-    // IE8 triggers resize on body size change, so they might not be
-    var hasSizes = this.size && size;
-    var innerSize = this.options.isHorizontal ? 'innerHeight' : 'innerWidth';
-    return hasSizes && size[ innerSize ] != this.size[ innerSize ];
+  // point to mode options for horizontal
+  var _getOption = proto._getOption;
+  proto._getOption = function( option ) {
+    if ( option == 'horizontal' ) {
+      return this.options.isHorizontal !== undefined ?
+        this.options.isHorizontal : this.options.horizontal;
+    }
+    return _getOption.apply( this.isotope, arguments );
   };
 
   return PackeryMode;
+
 }));
